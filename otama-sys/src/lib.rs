cec38f142b45ca36c8548fe34de371de9d9e54a1
Hideo Hattori <hattori-h@klab.com>
2017-05-10 22:13:44 +0900

    change construction

---
+++ otama-sys/src/lib.rs
@@ -0,0 +1,21 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::mem;
+    use std::ffi::CString;
+
+    #[test]
+    fn test_otama_open() {
+        let config = CString::new("path.yaml").unwrap();
+        unsafe {
+            let mut o = mem::zeroed();
+            otama_open(&mut o as *mut *mut otama_t, config.as_ptr());
+        }
+    }
+}
