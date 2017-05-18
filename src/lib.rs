020b548d372c532b0388d16d8146ec21c589a43e
Hideo Hattori <hattori-h@klab.com>
2017-05-18 11:03:51 +0900

    support insert, pull, search

--- src/lib.rs
+++ src/lib.rs
@@ -71,16 +71,29 @@ impl Otama {
         let f = ffi::CString::new(file).unwrap();
         unsafe {
             let mut id = mem::uninitialized();
-            let mut c_hexid = mem::zeroed();
+            let mut c_hexid: Vec<u8> = vec![0 as u8; 41];  // OTAMA_ID_HEXSTR_LEN
             match otama_insert_file(self.0 as *mut _, &mut id, f.as_ptr()) {
                 otama_status_t::OTAMA_STATUS_OK => {
-                    otama_id_bin2hexstr(&mut c_hexid as *mut _, &mut id);
-                    // FIXME: segv
-                    //let s = ffi::CString::from_raw(c_hexid as *mut ::std::os::raw::c_char);
-                    //let hexid = String::from(s.to_str().unwrap());
-                    let hexid = "hoge".to_string();
+                    otama_id_bin2hexstr(c_hexid.as_mut_ptr() as *mut i8, &mut id);
+                    c_hexid.truncate(40);
+                    let s = ffi::CString::new(c_hexid).unwrap();
+                    let hexid = String::from(s.to_str().unwrap());
                     Ok(hexid)
                 },
+                otama_status_t::OTAMA_STATUS_NODATA => Err(Error::Nodata),
+                otama_status_t::OTAMA_STATUS_INVALID_ARGUMENTS => Err(Error::InvalidArguments),
+                otama_status_t::OTAMA_STATUS_ASSERTION_FAILURE => Err(Error::AssertionFailure),
+                otama_status_t::OTAMA_STATUS_SYSERROR => Err(Error::SysError),
+                otama_status_t::OTAMA_STATUS_NOT_IMPLEMENTED => Err(Error::NotImplemented),
+                _ => Err(Error::Unknown),
+            }
+        }
+    }
+
+    pub fn pull(&mut self) -> Result<Status, Error> {
+        unsafe {
+            match otama_pull(self.0 as *mut _) {
+                otama_status_t::OTAMA_STATUS_OK => Ok(Status::Ok),
                 otama_status_t::OTAMA_STATUS_NODATA => Err(Error::Nodata),
                 otama_status_t::OTAMA_STATUS_INVALID_ARGUMENTS => Err(Error::InvalidArguments),
                 otama_status_t::OTAMA_STATUS_ASSERTION_FAILURE => Err(Error::AssertionFailure),
@@ -99,21 +112,22 @@ impl Otama {
                 otama_status_t::OTAMA_STATUS_OK => {
                     let mut r: Vec<OtamaResult> = Vec::new();
                     let n = otama_result_count(results as *mut _);
-                    println!("search. n={}", n);
                     for i in 0..n {
-                        let c_hexid = ffi::CString::new("").unwrap();
+                        let mut c_hexid: Vec<u8> = vec![0 as u8; 41];  // OTAMA_ID_HEXSTR_LEN
                         let id = otama_result_id(results as *mut _, i);
-                        otama_id_bin2hexstr(c_hexid.as_ptr() as *mut _, id);
-                        let hexid = String::from(c_hexid.to_str().unwrap());
+                        otama_id_bin2hexstr(c_hexid.as_mut_ptr() as *mut i8, id);
+                        c_hexid.truncate(40);
+                        let s = ffi::CString::new(c_hexid).unwrap();
+                        let hexid = String::from(s.to_str().unwrap());
 
                         let otama_result = otama_result_value(results as *mut _, i);
-                        //let similarity = self.get_similarity_from_result(otama_result);
                         let similarity = match self.variant2obj(otama_result) {
                             Value::Hash(v) => {
-                                //println!("{:?}", v);
-                                println!("hoge");
-                                let _ = v.get("similarity").unwrap();
-                                    0.0
+                                let sim = v.get("similarity").unwrap();
+                                match *sim {
+                                    Value::Float(v) => v,
+                                    _ => 0.0
+                                }
                             },
                             _ => 0.0,
                         };
@@ -154,18 +168,15 @@ impl Otama {
                     let cnt = otama_variant_array_count(result);
                     let mut v: Vec<Value> = Vec::new();
                     for i in 0..cnt {
-                        println!("{:?}", i);
                         v.push(self.variant2obj(otama_variant_array_at(result, i)));
                     }
                     Value::Array(v)
                 },
                 otama_variant_type_e::OTAMA_VARIANT_TYPE_HASH => {
-                    println!("hash");
                     let keys = otama_variant_hash_keys(result);
                     let cnt = otama_variant_array_count(result);
                     let mut h: HashMap<String, Value> = HashMap::new();
                     for i in 0..cnt {
-                        println!("{:?}", i);
                         let v = self.variant2obj(otama_variant_hash_at2(result, otama_variant_array_at(keys, i)));
                         let c_k = otama_variant_to_string(otama_variant_array_at(keys, i));
                         let k = ffi::CString::from_raw(c_k as *mut _);
