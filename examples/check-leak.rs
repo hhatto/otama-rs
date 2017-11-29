20afa373e1e2321be9b602cb11e5607b826b0024
Hideo Hattori <hattori-h@klab.com>
2017-11-29 12:33:24 +0900

    add to memory leak check script

---
+++ examples/check-leak.rs
@@ -0,0 +1,55 @@
+extern crate otama;
+extern crate glob;
+
+use std::thread::sleep;
+use std::time::Duration;
+
+fn main() {
+    let config_file = ::std::env::args().nth(1).expect("argument error");
+    let mut o = otama::Otama::new(config_file.as_str()).expect("otama error");
+    println!("otama={:?}", o);
+
+    match o.create_database() {
+        Err(e) => println!("create_database() error. e={:?}", e),
+        _ => {},
+    }
+
+    for entry in glob::glob("./image/*.[jp][pn]g").expect("invalid glob pattern") {
+        match entry {
+            Ok(path) => {
+                match o.insert(path.to_str().unwrap()) {
+                    Err(e) => println!("insert() error. e={:?}", e),
+                    Ok(id) => println!("insert id={:?} file={:?}. ", id, path),
+                }
+            },
+            Err(e) => println!("error={:?}", e),
+        }
+    }
+
+    match o.pull() {
+        Err(e) => println!("pull() error. e={:?}", e),
+        _ => {},
+    }
+
+    match o.search(10, "./image/lena.jpg") {
+        Err(e) => println!("search() error. e={:?}", e),
+        Ok(v) => {
+            for ret in v {
+                println!("{:?}", ret);
+            }
+        },
+    }
+
+    for _ in 0..1000 {
+        match o.search(10, "./image/lena.jpg") {
+            Err(e) => println!("search() error. e={:?}", e),
+            Ok(v) => {
+                for ret in v {
+                    let _ = ret;
+                }
+            },
+        }
+
+        sleep(Duration::from_millis(250));
+    }
+}
